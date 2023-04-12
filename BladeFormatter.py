c1dba3f82f694ce6b962e1cb2d6af7080a84d057
Rushabh Joshi <rushabh.r.joshi@gmail.com>
2023-04-12 16:17:56 +0530

    Format current file by using cli command

--- BladeFormatter.py
+++ BladeFormatter.py
@@ -1,7 +1,11 @@
 import sublime
 import sublime_plugin
+import os
 
 
 class BladeFormatterCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		self.view.insert(edit, 0, self.view.setting().get("syntax"))
+		file_name = self.view.file_name()
+		command = "blade-formatter --write " + file_name
+		if "blade.php" in file_name:
+			os.system(command)
