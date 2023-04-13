618a974bb4f1bdb439c1d9200e538ba6d3b507ce
Rushabh Joshi <rushabh.r.joshi@gmail.com>
2023-04-13 17:08:50 +0530

    Using stdin and stdout for better performance

--- BladeFormatter.py
+++ BladeFormatter.py
@@ -5,12 +5,14 @@ import subprocess
 
 class BladeFormatterCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		file_name = self.view.file_name()
-		command = ["blade-formatter", "--write", file_name]
+		whole_region = sublime.Region(0, self.view.size())
+		text = self.view.substr(whole_region)
+		command = ["blade-formatter", "--stdin"]
 		if self.view.file_name().endswith('.blade.php'):
-			subprocess.Popen(command)
+			stdout, stderr = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE).communicate(input=text.encode())
+			self.view.replace(edit, whole_region, stdout.decode())
 
 class BladeFormatterListener(sublime_plugin.EventListener):
-    def on_post_save(self, view):
+    def on_pre_save(self, view):
         if view.file_name().endswith('.blade.php'):
             view.run_command('blade_formatter')
