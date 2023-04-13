9850ff9a10edc7699667953b40a5db584cee5e92
Rushabh Joshi <rushabh.r.joshi@gmail.com>
2023-04-13 16:50:27 +0530

    Use subprocess instead of os

--- BladeFormatter.py
+++ BladeFormatter.py
@@ -1,16 +1,16 @@
 import sublime
 import sublime_plugin
-import os
+import subprocess
 
 
 class BladeFormatterCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		file_name = self.view.file_name()
-		command = "blade-formatter --write " + file_name
+		command = ["blade-formatter", "--write", file_name]
 		if self.view.file_name().endswith('.blade.php'):
-			os.system(command)
+			subprocess.Popen(command)
 
-class MySaveListener(sublime_plugin.EventListener):
+class BladeFormatterListener(sublime_plugin.EventListener):
     def on_post_save(self, view):
         if view.file_name().endswith('.blade.php'):
             view.run_command('blade_formatter')
